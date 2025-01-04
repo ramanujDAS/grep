1a13aed249e77dfa1c664557ed92f1595280d57f
ramanujsingh <ramanuj1st@gmail.com>
2025-01-04 23:10:25 +0530

    random

--- .codecrafters/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -e # Exit on failure
-
-mvn -B package -Ddir=/tmp/codecrafters-build-grep-java
--- .codecrafters/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-
-set -e # Exit on failure
-
-exec java -jar /tmp/codecrafters-build-grep-java/codecrafters-grep.jar "$@"
